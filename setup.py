bc0d4a8136591ece5079c3c5d5762b1d4f901cec
Xavier Ruiz <ruiz.xvr@gmail.com>
2018-10-12 15:38:08 +0200

    1_MAJ Git config 2_Structure 3_Version initiale non fonctionnelle 4_gitignore

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='mowitnow',
+      version='0.1',
+      description='MowItNow controller',
+      url='https://github.com/xavbyme/mowitnow.git',
+      author='XRU',
+      author_email='ruiz.xvr@gmail.com',
+      license='MIT',
+      packages=['mowitnow'],
+      zip_safe=False)
