542b36a95a8aef6e4852dee37814099fa2663dcb
Xavier Ruiz <ruiz.xvr@gmail.com>
2018-10-15 07:49:59 +0200

    Maj packaging

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(name='mowitnow',
-      version='0.2',
+      version='0.3',
       description='MowItNow controller',
       url='https://github.com/xavbyme/mowitnow.git',
       author='XRU',
